036c4e942c29df1826b4b42f5bce5f6f8a0610fa
KirillMehtiev <kirill.mehtiev@gmail.com>
2015-09-01 21:48:48 +0300

    хелпер для pageLinks

---
+++ BoublikSystem/HtmlHelpers/PagingHelpers.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+
+namespace BoublikSystem.HtmlHelpers
+{
+    public static class PagingHelpers
+    {
+        public static MvcHtmlString PageLinks(this HtmlHelper helper, string url, int pageCount, int currentPage, string updateId)
+        {
+            StringBuilder result = new StringBuilder();
+
+            if (pageCount != 1)
+            {
+                for (int i = 0; i < pageCount; i++)
+                {
+                    TagBuilder tag = new TagBuilder("a");
+                    tag.MergeAttribute("href", string.Format("{0}{1}", url, i));
+                    tag.MergeAttribute("data-ajax", "true");
+                    tag.MergeAttribute("data-ajax-mode", "replace");
+                    tag.MergeAttribute("data-ajax-update", updateId);
+                    tag.InnerHtml = (i + 1).ToString();
+                    if (i == currentPage)
+                    {
+                        tag.AddCssClass("selected");
+                        tag.AddCssClass("btn-primary");
+                    }
+                    tag.AddCssClass("btn btn-default");
+                    result.Append(tag.ToString());
+                }
+            }
+
+            return MvcHtmlString.Create(result.ToString());
+        }
+    }
+}
+
